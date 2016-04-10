dbd7505d88c6ab797ab79c539fb6b9d30d2e57bd
webere@lafayette.edu <admin@cognizance.us>
2016-04-09 22:08:53 -0400

    Added EndVsWinExperiment.

--- EndVsWinExperiment.java
+++ EndVsWinExperiment.java
@@ -1,12 +1,12 @@
 import java.util.*;
 import java.io.*;
 /**
- * AllPlayExperiment tests which of the four players has the highest percent of wins as points for pieces increase relative to points for winning.
+ * EndVsWinExperiment tests how often each type of player that wins the game also owns the token that ends the game, as the number of points for pieces increase relative to the number of points for winning.
  * 
  * @Eric Weber
  * @4/9/16
  */
-public class AllPlayExperiment {
+public class EndVsWinExperiment {
     
     /**
      * Run and analyze games of Chutes and Ladders and Pots.
@@ -16,12 +16,12 @@ public class AllPlayExperiment {
             final int j = i;
             new Thread() {
                 public void run() {
-                    File file = new File("Experiment2/output" + j + ".csv");
+                    File file = new File("Experiment3/output" + j + ".csv");
                     try {
                         file.createNewFile();
                         FileWriter printer = new FileWriter(file);
                         ArrayList<Integer> output = runExperiment(j);
-                        printer.write(j + ", " + output.get(0) + ", " + output.get(1) + ", " + output.get(2) + ", " + output.get(3));
+                        printer.write(j + ", " + output.get(0) + ", " + output.get(1) + ", " + output.get(2) + ", " + output.get(3) + ", " + output.get(4) + ", " + output.get(5) + ", " + output.get(6) + ", " + output.get(7));
                         printer.close();
                     }
                     catch (Exception e) {
@@ -40,7 +40,11 @@ public class AllPlayExperiment {
      * @return game winners
      */
     public static ArrayList<Integer> runExperiment(int eNumber) {
-        ArrayList<Integer> winTimes = new ArrayList<Integer>(4);
+        ArrayList<Integer> winTimes = new ArrayList<Integer>(8);
+        winTimes.add(0);
+        winTimes.add(0);
+        winTimes.add(0);
+        winTimes.add(0);
         winTimes.add(0);
         winTimes.add(0);
         winTimes.add(0);
@@ -86,13 +90,25 @@ public class AllPlayExperiment {
                     winTimes.set(0, winTimes.get(0) + 1);
                 }
                 else if (wonPlayer instanceof JustPointsPlayer) {
-                    winTimes.set(1, winTimes.get(1) + 1);
-                }
-                else if (wonPlayer instanceof TotalJerkPlayer) {
                     winTimes.set(2, winTimes.get(2) + 1);
                 }
+                else if (wonPlayer instanceof TotalJerkPlayer) {
+                    winTimes.set(4, winTimes.get(4) + 1);
+                }
                 else if (wonPlayer instanceof JustInFrontPlayer) {
+                    winTimes.set(6, winTimes.get(6) + 1);
+                }
+                if (wonPlayer instanceof FinishFirstPlayer && wonPlayer.getHighestIndex() == numberOfSpaces - 1) {
+                    winTimes.set(1, winTimes.get(1) + 1);
+                }
+                else if (wonPlayer instanceof JustPointsPlayer && wonPlayer.getHighestIndex() == numberOfSpaces - 1) {
                     winTimes.set(3, winTimes.get(3) + 1);
+                }
+                else if (wonPlayer instanceof TotalJerkPlayer && wonPlayer.getHighestIndex() == numberOfSpaces - 1) {
+                    winTimes.set(5, winTimes.get(5) + 1);
+                }
+                else if (wonPlayer instanceof JustInFrontPlayer && wonPlayer.getHighestIndex() == numberOfSpaces - 1) {
+                    winTimes.set(7, winTimes.get(7) + 1);
                 }
             }
             catch (Exception e) {
